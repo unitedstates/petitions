5f7405f56eaea455c399e3de694c09dffe3b1e22
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-01-19 21:44:17 -0500

    remove unused imports and PEP8 compliance

--- scripts/petitions.py
+++ scripts/petitions.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
-
+from StringIO import StringIO
 import json
-import os, sys
 import datetime
 import scrapelib
-from StringIO import StringIO
-from lxml.html import etree, parse
-from urlparse import urlparse
+
+from lxml.html import etree
 from utils import log, download, write, flags
-import urllib
-from optparse import OptionParser
+
 
 #intialize scraper and parser
 s = scrapelib.Scraper(requests_per_minute=60, follow_robots=False)
 parser = etree.HTMLParser()
 
-def petitions(mx=-1, start=1):    
+
+def petitions(mx=-1, start=1):
     hits = 0
     #scan WH site, add any new petitions to DB
     #surely a better way to get indefinite number of results than to create a functionally infinite loop, then breaking it, but drawing a blank
-    for pg in range(start,1000):
+    for pg in range(start, 1000):
         log("Loading page %d" % pg)
-        
+
         #The WH site loads petitions from an external HTML doc in a JSON shell
         url = "https://petitions.whitehouse.gov/petitions/more/all/%d/2/0/" % pg
         try:
             raw = s.urlopen(url).encode('utf-8')
-        except scrapelib.HTTPError as e:
+        except scrapelib.HTTPError:
             log("Error downloading %s" % url)
             return hits
         resp = json.loads(raw)
@@ -36,28 +34,29 @@ def petitions(mx=-1, start=1):
         page = etree.parse(StringIO(resp['markup']), parser)
         #there are two links to each petition in the results, but can reduce to uniques with "nofollow"
 
-        for petition in page.xpath("body/div[@class]"):        
+        for petition in page.xpath("body/div[@class]"):
             pid = petition.xpath("@id")[0].split('-')[1]
             #get uid for each petition from main div id
             path = petition.xpath("div/div/a/@href")[0]
             data = crawl(path, pid)
             #TO DO: add --fast flag for ignore existing petitions
-            write(json.dumps(data, indent=2, sort_keys=True), path.split("/")[2] + ".json") 
+            write(json.dumps(data, indent=2, sort_keys=True), path.split("/")[2] + ".json")
             hits += 1
             if mx != -1 and hits >= mx:
                 return hits
 
+
 #visit the page for each petition and get the vitals
 def crawl(path, pid=None):
     body = download("http://petitions.whitehouse.gov" + path, path.split('/')[2] + ".html")
-    page = etree.parse(StringIO(body), parser)    
+    page = etree.parse(StringIO(body), parser)
     raw_date = page.xpath("//div[@class='date']/text()")[0].strip()
-    created = datetime.datetime.strptime(raw_date, "%b %d, %Y").strftime("%Y-%m-%d")    
+    created = datetime.datetime.strptime(raw_date, "%b %d, %Y").strftime("%Y-%m-%d")
     signatures = page.xpath("//div[@class='num-block num-block2']/text()")
     signatures = int(signatures[0].replace(",", ''))
-    
+
     if not pid:
-        pid = page.xpath("//a[@class='load-next no-follow active']/@rel")[0]        
+        pid = page.xpath("//a[@class='load-next no-follow active']/@rel")[0]
     return {
         "pid": pid,
         "title": page.xpath("//h1[@class='title']/text()")[0].strip(),
@@ -68,11 +67,13 @@ def crawl(path, pid=None):
         "signatures": signatures,
         "url": "http://petitions.whitehouse.gov" + path
     }
-    
+
+
 def main():
     start = flags().get('start', 1)
     mx = flags().get('max', -1)
     log("Found %i petitions" % (petitions(mx, start)))
-    
+
+
 if __name__ == "__main__":
     main()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,165 +1,172 @@
-import os, os.path, errno, sys, traceback
-import json
-import re
-import htmlentitydefs
-import pprint
-import datetime, time
-from lxml import html, etree
 from pprint import pprint
-import logging
+import datetime
+import errno
+import htmlentitydefs
+import os
+import os.path
+import re
+import sys
+
 from pytz import timezone
-
-import smtplib
-import email.utils
-from email.mime.text import MIMEText
-import getpass
-
 
 # read in an opt-in config file for changing directories and supplying email settings
 # returns None if it's not there, and this should always be handled gracefully
 
 eastern_time_zone = timezone('US/Eastern')
 
+
 def log(object):
-  if isinstance(object, (str, unicode)):
-    print object
-  else:
-    pprint(object)
-    
+    if isinstance(object, (str, unicode)):
+        print object
+    else:
+        pprint(object)
+
+
 def format_datetime(obj):
-  if isinstance(obj, datetime.datetime):
-    return eastern_time_zone.localize(obj.replace(microsecond=0)).isoformat()
-  elif isinstance(obj, str):
-    return obj
-  else:
-    return None
+    if isinstance(obj, datetime.datetime):
+        return eastern_time_zone.localize(obj.replace(microsecond=0)).isoformat()
+    elif isinstance(obj, str):
+        return obj
+    else:
+        return None
+
 
 def write(content, destination):
-  data_path = os.path.join(data_dir(), destination)     
-  mkdir_p(os.path.dirname(data_path))
-  f = open(data_path, 'w')
-  f.write(content)
-  f.close()
+    data_path = os.path.join(data_dir(), destination)
+    mkdir_p(os.path.dirname(data_path))
+    f = open(data_path, 'w')
+    f.write(content)
+    f.close()
+
 
 def read(destination):
-  if os.path.exists(destination):
-    with open(destination) as f:
-      return f.read()
+    if os.path.exists(destination):
+        with open(destination) as f:
+            return f.read()
+
 
 # de-dupe a list, taken from:
 # http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
 def uniq(seq):
-  seen = set()
-  seen_add = seen.add
-  return [ x for x in seq if x not in seen and not seen_add(x)]
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if x not in seen and not seen_add(x)]
 
-import os, errno
 
 # mkdir -p in python, from:
 # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
 def mkdir_p(path):
-  try:
-    os.makedirs(path)
-  except OSError as exc: # Python >2.5
-    if exc.errno == errno.EEXIST:
-      pass
-    else: 
-      raise
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+
 
 def xpath_regex(doc, element, pattern):
-  return doc.xpath(
-    "//%s[re:match(text(), '%s')]" % (element, pattern), 
-    namespaces={"re": "http://exslt.org/regular-expressions"})
+    return doc.xpath(
+        "//%s[re:match(text(), '%s')]" % (element, pattern),
+        namespaces={"re": "http://exslt.org/regular-expressions"})
+
 
 # taken from http://effbot.org/zone/re-sub.htm#unescape-html
 def unescape(text):
 
-  def remove_unicode_control(str):
-    remove_re = re.compile(u'[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F]')
-    return remove_re.sub('', str)
+    def remove_unicode_control(str):
+        remove_re = re.compile(u'[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F]')
+        return remove_re.sub('', str)
 
-  def fixup(m):
-    text = m.group(0)
-    if text[:2] == "&#":
-      # character reference
-      try:
-        if text[:3] == "&#x":
-          return unichr(int(text[3:-1], 16))
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
         else:
-          return unichr(int(text[2:-1]))
-      except ValueError:
-        pass
-    else:
-      # named entity
-      try:
-        text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-      except KeyError:
-        pass
-    return text # leave as is
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text  # leave as is
 
-  text = re.sub("&#?\w+;", fixup, text)
-  text = remove_unicode_control(text)
-  return text
+    text = re.sub("&#?\w+;", fixup, text)
+    text = remove_unicode_control(text)
+    return text
 
 
-##### Downloading 
+##### Downloading
 #the os.getcwd() (current working directory) is for file system access in Cloud9IDE
 
 
 import scrapelib
 scraper = scrapelib.Scraper(requests_per_minute=120, follow_robots=False, retry_attempts=3)
 
+
 # uses config values if present
 def cache_dir():
-  return os.getcwd() + "/cache"
+    return os.getcwd() + "/cache"
+
 
 # uses config values if present
 def data_dir():
-  return os.getcwd() + "/data"
+    return os.getcwd() + "/data"
+
 
 def download(url, destination, force=False, options=None):
-  if not options:
-    options = {}
+    if not options:
+        options = {}
 
-  cache = os.path.join(cache_dir(), destination) 
-  
-  if not force and os.path.exists(cache):
-    if options.get('debug', False):
-      log("Cached: (%s, %s)" % (cache, url))
+    cache = os.path.join(cache_dir(), destination)
 
-    with open(cache, 'r') as f:
-      body = f.read()
-  else:
-    try:
-      if options.get('debug', False):
-        log("Downloading: %s" % url)
-      response = scraper.urlopen(url)
-      body = response.encode('utf-8')
-    except scrapelib.HTTPError as e:
-      log("Error downloading %s" % url)
-      return None
+    if not force and os.path.exists(cache):
+        if options.get('debug', False):
+            log("Cached: (%s, %s)" % (cache, url))
 
-    # don't allow 0-byte files
-    if (not body) or (not body.strip()):
-      return None
+        with open(cache, 'r') as f:
+            body = f.read()
+    else:
+        try:
+            if options.get('debug', False):
+                log("Downloading: %s" % url)
+            response = scraper.urlopen(url)
+            body = response.encode('utf-8')
+        except scrapelib.HTTPError:
+            log("Error downloading %s" % url)
+            return None
 
-    # cache content to disk
-    write(body, cache)
+        # don't allow 0-byte files
+        if (not body) or (not body.strip()):
+            return None
 
-  return body
+        # cache content to disk
+        write(body, cache)
+
+    return body
+
 
 def flags():
-  options = {}
-  for arg in sys.argv[1:]:
-    if arg.startswith("--"):
-      if "=" in arg:
-        key, value = arg.split('=')
-      else:
-        key, value = arg, True
-      
-      key = key.split("--")[1]
-      if value == 'True': value = True
-      elif value == 'False': value = False
-      elif re.sub("\d+", "", value) == "": value = int(value)
-      options[key.lower()] = value
-  return options
+    options = {}
+    for arg in sys.argv[1:]:
+        if arg.startswith("--"):
+            if "=" in arg:
+                key, value = arg.split('=')
+            else:
+                key, value = arg, True
+
+            key = key.split("--")[1]
+            if value == 'True':
+                value = True
+            elif value == 'False':
+                value = False
+            elif re.sub("\d+", "", value) == "":
+                value = int(value)
+            options[key.lower()] = value
+    return options
